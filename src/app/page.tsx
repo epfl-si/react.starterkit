29e448525ae9d7789c2d45808b7a937b9d273546
SaphireVert <jerome.cosandey@gmail.com>
2023-04-13 15:45:12 +0200

    feat: add react-appauth login button
    Integration based on `epfl-si/react-appauth` solution.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,21 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import { Base } from '@epfl/epfl-sti-react-library'
+import { OIDCContext, StateEnum, LoginButton, IfOIDCState, LoggedInUser } from '@epfl-si/react-appauth'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <Base asideMenuItems={[]}>
-      This is a base page
+      <OIDCContext authServerUrl = { 'http://localhost:8080/realms/react-starter-kit/' }
+                      client = { { clientId: "react-starter-kit",
+                                   redirectUri: "http://localhost:3000/" } }>
+      <LoginButton inProgressLabel={ <>⏳</> }/>
+      <IfOIDCState is={ StateEnum.LoggedIn }>
+        <p>Hello, <LoggedInUser field="preferred_username" />!</p>
+      </IfOIDCState>
+    </OIDCContext>
     </Base>
   )
 }
